f729f6208f89c0fa64f0fdba69d0f90c1e3d7eb6
Dhaboav <100108392+Dhaboav@users.noreply.github.com>
2025-02-11 20:35:12 +0700

    refactor (file): Separate rename and extension validation logic

--- examples/label_drawer_demo.py
+++ examples/label_drawer_demo.py
@@ -1,23 +1,10 @@
-import cv2
-
-from src.label_drawer import yolo_to_opencv
+from src.file import retrieve_files
 
 if __name__ == "__main__":
-    datasets_path = (
-        r"datasets\train\images\data-1_png.rf.f87531b8bf3c3e204809be92c509f0a8.jpg"
-    )
-    # Load the image
-    image = cv2.imread(datasets_path)
+    datasets_path = r"datasets/train/images"
+    label_path = r"datasets/train/labels"
 
-    x, y, bboxw, bboxh = 0.18125, 0.334375, 0.058333333333333334, 0.10104166666666667
+    images = retrieve_files(datasets_path, extensions=["jpg"])
+    annotations = retrieve_files(label_path, extensions=["txt"])
 
-    # Get dimensions
-    image_height, image_width = image.shape[:2]
-    x_min, y_min, x_max, y_max = yolo_to_opencv(
-        x, y, bboxw, bboxh, image_width, image_height
-    )
-    cv2.rectangle(image, (x_min, y_min), (x_max, y_max), (0, 255, 0), 2)
-    cv2.imshow("t", image)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
-    print(x_min, y_min)
+    print(len(images), len(annotations))
--- src/file.py
+++ src/file.py
@@ -1,6 +1,40 @@
 import os
 
 
+def get_files(folder_path: str) -> list[str] | None:
+    """
+    Retrieves a list of files in the specified folder.
+
+    Args:
+        folder_path (str): Path to the folder.
+
+    Returns:
+        list[str]: List of file paths if the folder exists, or None if invalid.
+    """
+    if not os.path.isdir(folder_path):
+        return None
+    return [
+        os.path.join(folder_path, file)
+        for file in os.listdir(folder_path)
+        if os.path.isfile(os.path.join(folder_path, file))
+    ]
+
+
+def is_valid_extension(file_path: str, extensions: list[str]) -> bool:
+    """
+    Checks if a file has a valid extension.
+
+    Args:
+        file_path (str): Path to the file.
+        extensions (list[str]): List of valid extensions.
+
+    Returns:
+        bool: True if the file has a valid extension, False otherwise.
+    """
+    _, file_extension = os.path.splitext(file_path)
+    return file_extension.lower().lstrip(".") in extensions
+
+
 def rename_file(
     file_path: str, new_name: str, extensions: list[str], counter: int = 0
 ) -> bool:
@@ -16,39 +50,53 @@ def rename_file(
     Returns:
         bool: True if the file was successfully renamed, False otherwise.
     """
-    directory, file_name = os.path.split(file_path)
-    _, file_extension = os.path.splitext(file_name)
-
-    # Check for valid file extension
-    if file_extension.lower().lstrip(".") not in extensions:
+    if not is_valid_extension(file_path, extensions):
         return False
+
+    directory, _ = os.path.split(file_path)
+    _, file_extension = os.path.splitext(file_path)
 
     # Generate new file name and path
     new_file_name = f"{new_name}-{counter}{file_extension}"
     new_file_path = os.path.join(directory, new_file_name)
 
-    # Skip if the new file already exists
     if os.path.exists(new_file_path):
         return False
 
-    os.rename(file_path, new_file_path)
-    return True
+    return _perform_rename(file_path, new_file_path)
 
 
-def get_files(folder_path: str) -> list[str] | None:
+def retrieve_files(folder_path: str, extensions: list[str]) -> list[str]:
     """
-    Retrieves a list of files in the specified folder.
+    Retrieves files from a folder that match the valid extensions.
 
     Args:
         folder_path (str): Path to the folder.
+        extensions (list[str]): List of valid extensions.
 
     Returns:
-        list[str] | None: List of file paths if the folder exists, or None if invalid.
+        list[str]: List of files with valid extensions, or an empty list if none found.
     """
-    if not os.path.isdir(folder_path):
-        return None
-    return [
-        os.path.join(folder_path, file)
-        for file in os.listdir(folder_path)
-        if os.path.isfile(os.path.join(folder_path, file))
-    ]
+    files = get_files(folder_path)
+    if not files:
+        return []
+
+    return [file for file in files if is_valid_extension(file, extensions)]
+
+
+def _perform_rename(source: str, destination: str) -> bool:
+    """
+    Performs the actual renaming operation.
+
+    Args:
+        source (str): Current file path.
+        destination (str): New file path.
+
+    Returns:
+        bool: True if the renaming was successful, False otherwise.
+    """
+    try:
+        os.rename(source, destination)
+        return True
+    except Exception as e:
+        return False
